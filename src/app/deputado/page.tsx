4649c09e08f3fda8b38cd8430a53e31d89f9f56f
Tiago <tiagofleischauer@hotmail.com>
2023-12-12 11:28:56 -0300

    feat: casos de dados vazios

--- src/app/deputado/page.tsx
+++ src/app/deputado/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import { useEffect, useState } from "react";
@@ -34,28 +35,34 @@ export default function Deputado() {
     <main className="flex flex-1 p-6">
       <Title text="Deputado" />
       <div className="flex flex-1 gap-24 justify-center items-center">
-        {deputadoSelecionado && (
-          <div className="flex flex-col gap-2 w-72 bg-white/5 p-3 rounded-md">
-            <Image
-              src="/test.jpg"
-              className="w-full"
-              width={256}
-              height={256}
-              alt={`Deputado ${deputadoSelecionado.nomeCivil}`}
-            />
-            <div className="flex flex-col gap-1">
-              <strong className="font-semibold text-2xl">
-                {deputadoSelecionado.nomeCivil}
-              </strong>
-              <span className="text-lg text-zinc-400">{`Partido: ${deputadoSelecionado.siglaPartido}`}</span>
-              <span className="text-lg text-zinc-400">{`UF: ${deputadoSelecionado.siglaUf}`}</span>
+        {deputadoSelecionado ? (
+          <>
+            <div className="flex flex-col gap-2 w-72 bg-white/5 p-3 rounded-md">
+              <Image
+                src="/test.jpg"
+                className="w-full"
+                width={256}
+                height={256}
+                alt={`Deputado ${deputadoSelecionado.nomeCivil}`}
+              />
+              <div className="flex flex-col gap-1">
+                <strong className="font-semibold text-2xl">
+                  {deputadoSelecionado.nomeCivil}
+                </strong>
+                <span className="text-lg text-zinc-400">{`Partido: ${deputadoSelecionado.siglaPartido}`}</span>
+                <span className="text-lg text-zinc-400">{`UF: ${deputadoSelecionado.siglaUf}`}</span>
+              </div>
             </div>
-          </div>
+            <div className="flex flex-col gap-6">
+              <Button label="Inscrever" path="" />
+              <Button label="Ver eventos" path="" color="SECONDARY" />
+            </div>
+          </>
+        ) : (
+          <strong className="font-semibold text-2xl">
+            Deputado não encontrado.
+          </strong>
         )}
-        <div className="flex flex-col gap-6">
-          <Button label="Inscrever" path="" />
-          <Button label="Ver eventos" path="" color="SECONDARY" />
-        </div>
       </div>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,32 +24,40 @@ export default function Home() {
   return (
     <main className="flex-1 p-6">
       <Title text="Deputados" />
-      <div className="grid grid-cols-8 gap-4 mt-4">
-        {deputados.map((deputado) => {
-          return (
-            <Link
-              key={deputado.id}
-              href={{
-                pathname: "/deputado",
-                query: { id: deputado.id },
-              }}
-              className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
-            >
-              <Image
-                src="/test.jpg"
-                className="w-full"
-                width={120}
-                height={120}
-                alt={`Deputado ${deputado.nomeCivil}`}
-              />
-              <strong className="font-semibold">{deputado.nomeCivil}</strong>
-              <span className="text-sm text-zinc-500">
-                {`${deputado.siglaPartido} | ${deputado.siglaUf}`}
-              </span>
-            </Link>
-          );
-        })}
-      </div>
+      {deputados.length > 0 ? (
+        <div className="grid grid-cols-8 gap-4 mt-4">
+          {deputados.map((deputado) => {
+            return (
+              <Link
+                key={deputado.id}
+                href={{
+                  pathname: "/deputado",
+                  query: { id: deputado.id },
+                }}
+                className="bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10"
+              >
+                <Image
+                  src="/test.jpg"
+                  className="w-full"
+                  width={120}
+                  height={120}
+                  alt={`Deputado ${deputado.nomeCivil}`}
+                />
+                <strong className="font-semibold">{deputado.nomeCivil}</strong>
+                <span className="text-sm text-zinc-500">
+                  {`${deputado.siglaPartido} | ${deputado.siglaUf}`}
+                </span>
+              </Link>
+            );
+          })}
+        </div>
+      ) : (
+        <div className="flex h-full justify-center items-center">
+          <strong className="font-semibold text-2xl">
+            Nenhum deputado encontrado.
+          </strong>
+        </div>
+      )}
     </main>
   );
 }
