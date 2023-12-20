096b5111f61afde2de1004febcc2cff0268b05aa
Tiago <tiagofleischauer@hotmail.com>
2023-12-19 22:54:04 -0300

    feat: botao de adicionar evento

--- src/app/eventosDeputado/page.tsx
+++ src/app/eventosDeputado/page.tsx
@@ -7,8 +7,9 @@ import { Title } from "../components/Title";
 import { useLayoutEffect, useState } from "react";
 import { Deputado } from "../types/deputado";
 import { deleteEvent, editEvent, fetchDeputado } from "../utils";
-import { Pencil, Trash2 } from "lucide-react";
+import { Pencil, Plus, Trash2 } from "lucide-react";
 import Link from "next/link";
+import { Button } from "../components/Button";
 
 export default function EventosDeputado() {
   const props = useSearchParams();
@@ -48,12 +49,12 @@ export default function EventosDeputado() {
       {deputadoSelecionado && deputadoSelecionado.eventos.length > 0 ? (
         <>
           <Title text={`Eventos de ${deputadoSelecionado.nome}`} />
-          <div className="grid grid-cols-5 gap-4 mt-4">
+          <div className="grid grid-cols-5 gap-4 mt-4 items-center">
             {deputadoSelecionado.eventos.map((evento) => {
               return (
                 <div
                   key={evento.id}
-                  className="bg-white/5 p-6 rounded-md flex gap-2 items-center justify-between"
+                  className="bg-white/5 p-6 rounded-md flex gap-2 h-full items-center justify-between"
                 >
                   <strong className="text-lg overflow-hidden text-ellipsis">
                     {evento.descricaoTipo}
@@ -90,15 +91,36 @@ export default function EventosDeputado() {
                 </div>
               );
             })}
+            <Link
+              href={{
+                pathname: "/addToEvent",
+                query: { id: deputadoSelecionado.id },
+              }}
+            >
+              <div className="bg-blue-500 hover:bg-blue-400 w-16 h-16 rounded-full flex items-center justify-center">
+                <Plus />
+              </div>
+            </Link>
           </div>
         </>
       ) : (
-        <div className="flex h-full justify-center items-center">
+        <div className="flex flex-col gap-6 h-full justify-center items-center">
           <strong className="font-semibold text-2xl">
             {`Nenhum evento cadastrado ${
               deputadoSelecionado ? `para ${deputadoSelecionado.nome}` : ""
             }`}
           </strong>
+          {deputadoSelecionado && (
+            <Button
+              label="Adicionar"
+              onPress={{
+                href: {
+                  pathname: "/addToEvent",
+                  query: { id: deputadoSelecionado.id },
+                },
+              }}
+            />
+          )}
         </div>
       )}
     </main>
